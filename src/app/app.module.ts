cf615640cf07c71f098b8b8690e47d29619e1d20
Pedro Lopez <fairy444>
2020-05-21 03:13:37 -0500

    rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './website/home/home.component';
+import { SociosComponent } from './website/socios/socios.component';
+import { HistoriaComponent } from './website/historia/historia.component';
+import { ContactoComponent } from './website/contacto/contacto.component';
+import {ClienteComponent} from './website/cliente/cliente.component';
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'socio', component: SociosComponent },
+  { path: 'historia', component: HistoriaComponent },
+  { path: 'cliente', component: ClienteComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SociosComponent,
+    HistoriaComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
