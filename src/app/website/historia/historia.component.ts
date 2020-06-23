6cba0545c620b9b90b61b3204ff7feb41e4c9d37
Pedro Lopez <fairy444>
2020-06-23 13:47:33 -0500

    nuevo orden en biografia

--- src/app/website/historia/historia.component.ts
+++ src/app/website/historia/historia.component.ts
@@ -1,7 +1,10 @@
-import { Component, Injector, OnInit } from '@angular/core';
-import { RoutedViewModelComponent }    from '../../core/routed-ViewModel.component';
+import {Component, Injector, OnInit} from '@angular/core';
+import {RoutedViewModelComponent} from '../../core/routed-ViewModel.component';
 
-class InitialVMState {}
+declare const $: any;
+
+class InitialVMState {
+}
 
 @Component({
   selector: 'app-historia',
@@ -13,5 +16,35 @@ export class HistoriaComponent extends RoutedViewModelComponent {
 
   constructor(private _LocalInjector: Injector) {
     super(_LocalInjector);
+  }
+
+  init() {
+    this.renderSlider();
+  }
+
+  renderSlider() {
+
+    $('.tp-perfil').show().owlCarousel({
+      stagePadding: 50,
+      loop: true,
+      margin: 10,
+      nav: true,
+      items: 1
+    });
+    $('.tp-video').show().owlCarousel({
+      stagePadding: 50,
+      loop: true,
+      margin: 10,
+      nav: true,
+      items: 1
+    });
+    $('.postimg').magnificPopup({
+      type: 'image',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      },
+    });
   }
 }
